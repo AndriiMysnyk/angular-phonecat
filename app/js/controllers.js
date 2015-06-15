2f333455b8ded0dafa784a59d27189ef9d24b34a
Andrii Mysnyk <ondron@gmail.com>
2015-06-15 23:47:00 +0300

    Phone filtering by carrier added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,6 +8,23 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
   function($scope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
+    $scope.carrierProp = 'all';
+
+    $scope.filterPhones = function(phone) {
+        switch ($scope.carrierProp) {
+            case "all":
+                return phone;
+
+            case "none":
+                return phone.carrier == undefined ? phone : null;
+
+            default:
+                if ($scope.carrierProp != phone.carrier) {
+                    return;
+                }
+                return phone;
+        }
+    }
   }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
